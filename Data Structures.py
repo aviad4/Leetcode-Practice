a7fd6c37db4d77d15f835455b110004a10aeae89
aviad <aviad442@gmail.com>
2024-08-21 17:07:29 +0300

    def get(self,index) - 20/08

--- Data Structures.py
+++ Data Structures.py
@@ -1,3 +1,6 @@
+from tkinter.messagebox import RETRY
+
+
 class Node:
     def __init__(self, value):  # Corrected constructor
         self.value = value
@@ -48,7 +51,7 @@ class LinkedList:
             self.tail = None
         return temp.value
 
-    def get_test(self, index):
+    def get(self, index): #מחזיר לי נוד
         if index < 0 or index >= self.length:
             return None
         temp = self.head
@@ -56,9 +59,65 @@ class LinkedList:
             temp = temp.next
         return temp
 
-my_linked_list = LinkedList(0)
-my_linked_list.append(1)
-my_linked_list.append(2)
-my_linked_list.append(3)
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
 
-print(my_linked_list.get_test(0))
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+
+    def remove(self, index):
+        if index < 0 or index > self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            self.pop()
+        prev = self.get(index - 1)
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+
+
+
+my_linked_list = LinkedList(11)
+my_linked_list.append(3)
+my_linked_list.append(24)
+my_linked_list.append(7)
+
+my_linked_list.print_list()
+
+my_linked_list.remove(2)
+my_linked_list.print_list()
